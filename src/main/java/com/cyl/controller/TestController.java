2b339b79d44fb93a7f1cc647cc38b2d4cf800903
cgynlai <cgynlai@gmail.com>
2021-11-11 20:10:33 +0800

    added RequestMapping and properties

--- src/main/java/com/cyl/controller/TestController.java
+++ src/main/java/com/cyl/controller/TestController.java
@@ -2,13 +2,20 @@ package com.cyl.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
 	
-	@RequestMapping("/test")
-	public String test( ) {
-		System.out.println("success!!");
+	@RequestMapping(value="/test", method=RequestMethod.GET )
+	public String testGET( ) {
+		System.out.println("success from GET!!");
+		return "success";
+	}
+	
+	@RequestMapping(value="/test", method=RequestMethod.POST )
+	public String testPOST( ) {
+		System.out.println("success from POST !!");
 		return "success";
 	}
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,5 +2,10 @@
 <body>
 <h2>Hello World!</h2>
    <a href="test">test spring mvc</a>
+   
+   <br />
+   <form action="test" method="post">
+       <input type="submit" value="POST test">
+   </form>
 </body>
 </html>
