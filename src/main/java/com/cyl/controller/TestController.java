5a05237ee7a5ea75cd61495b2860f96c548a2452
cgynlai <cgynlai@gmail.com>
2021-11-15 22:52:02 +0800

    cookie value

--- src/main/java/com/cyl/controller/TestController.java
+++ src/main/java/com/cyl/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.cyl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,5 +21,11 @@ public class TestController {
 		System.out.println("username= " + username + "password= " + password );
 		return "success";
 	}
-
+    
+	@RequestMapping(value="/test1", method=RequestMethod.GET )
+	public String testGET2(@CookieValue(value="JSESSIONID") String sessionID ) {
+		System.out.println("success from get2 !!");
+		System.out.println("session id = " + sessionID );
+		return "success";
+	}
 }
